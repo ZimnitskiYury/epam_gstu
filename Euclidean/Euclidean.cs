172f779583d1c3ae1b8b3d9a87caff9bc3e8928b
Юрий Зимницкий <demovie.inc@live.com>
2019-11-05 21:49:34 +0300

    Первая часть таска для двух переменных с помощью рекурсии

---
+++ Euclidean/Euclidean.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01
+{
+    public class Euclidean
+    {
+        //метод для получения Нод двух чисел
+        public virtual int GetNod(int n, int k)
+        {
+            int f;
+            int s;
+            int nod=0;
+            //проверка больше ли n>k
+            if (n > k)
+                //если да, то переменные получают соответствующие значения
+                //первая f равна n, вторая s равна k
+            {
+                f = n;
+                s = k;
+            }
+                //если нет, то меняем местами
+            else
+            {
+                f = k;
+                s = n;
+            }
+            //проверка на неравенство остатка нулю
+            //если неравен, то запускается этот же метод
+            //с новыми значениями
+            if (f % s != 0)
+            {
+                nod = GetNod(s, f % s);
+            }
+            else nod = s;
+            return nod;
+        }
+    }
+}
---
+++ task01Tests/EuclideanTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using task01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task01.Tests
+{
+    [TestClass()]
+    public class EuclideanTests
+    {
+        [TestMethod()]
+        //тест с обычными значениями
+        public void GetNodTest_1()
+        {
+            Euclidean test1 = new Euclidean();
+            int f = 100;
+            int s = 25;
+            int expected= 25;
+            int result=test1.GetNod(f, s);
+            EuclideanTests.Equals(expected, result);
+        }
+        [TestMethod()]
+        //тест с обратными
+        public void GetNodTest_2()
+        {
+            Euclidean test1 = new Euclidean();
+            int f = 25;
+            int s = 100;
+            int expected = 25;
+            int result = test1.GetNod(f, s);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        //тест с вызовом рекурсии
+        public void GetNodTest_3()
+        {
+            Euclidean test1 = new Euclidean();
+            int f = 120;
+            int s = 25;
+            int expected = 5;
+            int result = test1.GetNod(f, s);
+            Assert.AreEqual(expected, result);
+        }
+        [TestMethod()]
+        //тест с вызовом рекурсии
+        public void GetNodTest_4()
+        {
+            Euclidean test1 = new Euclidean();
+            int f = 219;
+            int s = 39;
+            int expected = 3;
+            int result = test1.GetNod(f, s);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
