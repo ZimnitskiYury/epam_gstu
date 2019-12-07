65c802ed43e12d140d1350d81d6a6f23f6b3c1e1
Юрий Зимницкий <demovie.inc@live.com>
2019-12-07 16:49:03 +0300

    box class layout

---
+++ task3/task3/Box.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    public class Box
+    {
+        List<Figure> boxoffigure = new List<Figure>(20);
+        //Добавление фигуры в коробку
+        public void AddFigure(Figure f1)
+        {
+            //Проверка на переполнение коробки. макс 20
+            if (boxoffigure.Count == 20)
+            {
+                //Исключение
+            }
+            else
+            {
+                boxoffigure.Add(f1);
+            }
+        }
+        //Просмотр фигуры без извлечения по номеру
+        public void ViewFigure()
+        {
+
+        }
+        //Извлечение фигуры по номеру
+        public void RemoveFigure()
+        {
+            //Проверка на фигуры в коробке?
+            if (boxoffigure.Count == 0)
+            {
+                //Исключение
+            }
+            else
+            {
+
+            }
+        }
+        //Замена фигуры на другую
+        public void ReplaceFigure(Figure f1, int i)
+        {
+            //проверка на существование фигуры
+            if (i >= CountFigures())
+            {
+                //исключение
+            }
+            else
+            {
+
+            }
+        }
+        //Поиск фигуры по характеристикам
+        public int FindFigure(Figure f1)
+        {
+            int i = 0;//переделать
+            return i;
+        }
+        //фигур в списке
+        public int CountFigures()
+        {
+            int i = 0;//переделать
+            return i;
+        }
+        /// <summary>
+        /// Общая площадь
+        /// </summary>
+        /// <returns></returns>
+        public float TotalArea()
+        {
+            float i = 0;//переделать
+            return i;
+        }
+        /// <summary>
+        /// Общий периметр
+        /// </summary>
+        /// <returns></returns>
+        public float TotalPerimeter()
+        {
+            float i = 0;//переделать
+            return i;
+        }
+        /// <summary>
+        /// Удалить круги
+        /// </summary>
+        public void RemoveCircles()
+        {
+
+        }
+        /// <summary>
+        /// Удалить пленки
+        /// </summary>
+        public void RemovePolyethylene()
+        {
+
+        }
+        /// <summary>
+        /// Save xml (streamwriter)
+        /// </summary>
+        public void SaveXML()
+        {
+
+        }
+        /// <summary>
+        /// load xml (streamreader)
+        /// </summary>
+        public void LoadXML()
+        {
+
+        }
+        /// <summary>
+        /// Save xml (xmlwriter)
+        /// </summary>
+        public void SaveXML2()
+        {
+
+        }
+        /// <summary>
+        /// load xml (xmlreader)
+        /// </summary>
+        public void LoadXML2()
+        {
+
+        }
+    }
+}
