5ffb794071d000e53ca69693b39d750a58cbbe64
Юрий Зимницкий <demovie.inc@live.com>
2019-12-29 21:05:51 +0300

    Realize Add()

--- test5/test5/BinaryTree.cs
+++ test5/test5/BinaryTree.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace test5
 {
-    class BinaryTree
+    class BinaryTree<T> where T : IComparable
     {
+        public Node<T> Root { get; set; }
+        public int Count { get; set; }
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (Root == null)
+            {
+                Root = node;
+                Count = 1;
+                return;
+            }
+            Add(node, Root);
+        }
+        public void Add(Node<T> _node, Node<T> _root)
+        {
+            if (_node.CompareTo(_root) == -1)
+            {
+                if (_root.Left == null)
+                {
+                    _root.Left = _node;
+                }
+                else Add(_node, _root.Left);
+            }
+            else
+            {
+                if (_root.Right == null)
+                    _root.Right = _node;
+                else
+                    Add(_node, _root);
+            }
+        }
     }
 }
--- test5/test5/Node.cs
+++ test5/test5/Node.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace test5
 {
-    class Node
+    class Node<T> where T : IComparable
     {
+        public T Data{get; set;}
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+        public int CompareTo(object obj)
+        {
+
+            if (obj is Node<T> name)
+            {
+                return this.Data.CompareTo(name.Data);
+            }
+            else throw new Exception("Wrong Input Node");
+        }
     }
 }
