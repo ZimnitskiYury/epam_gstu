45485ff5f0f1f9347d7dd4e75cc5d33b70e8370d
Юрий Зимницкий <demovie.inc@live.com>
2020-01-31 02:54:10 +0300

    Trying to do factory

---
+++ task6/DBA/Factory.cs
@@ -0,0 +1,16 @@
+﻿using StudentsAndGrades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbDao
+{
+    class Factory
+    {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Task6DB;";
+
+        
+    }
+}
---
+++ task6/DBA/IFactory.cs
@@ -0,0 +1,14 @@
+﻿using StudentsAndGrades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbDao
+{
+    interface IFactory<T>
+    {
+        IDao<T> Create();
+    }
+}
